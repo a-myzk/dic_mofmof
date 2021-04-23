7b50d0401d8df485bcff6816fc2171a1184a2ed6
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-04-23 15:55:28 +0900

    [Add]物件登録フォーム作成
    登録フォームのみ（最寄り駅フォームは未着手）

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,24 @@
 class Property < ApplicationRecord
+
 end
+
+
+# class Estate < ApplicationRecord
+#   has_many :stations、dependent: destroy
+#   accepts_nested_attributes_for :station, reject_if:
+#       proc {|attributes| attributes['line_name'].blank? and
+#       attributes['station_name'].blank? and 
+#       attributes['walk_time'].blank?}, allow_destroy: true
+# end
+
+#駅
+# class CreateStations < ActiveRecord::Migration[5.2]
+#   def change
+#     create_table :stations do |t|
+#       t.string :line_name
+#       t.string :station_name
+#       t.integer :walking_time
+#       t.references :estate, foreign_key: true
+#     end
+#   end
+# end
